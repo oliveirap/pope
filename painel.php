57d962d98690c979ade2c3feb52c763549fa3c8e
oliveirap <pedroliveira007@hotmail.com>
2015-09-12 00:35:32 -0300

    Configurações iniciais

---
+++ painel.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Plataform Online For Problems and Exercices - Plataforma online para exercicíos interativos voltadas para o ensino de Fisica">
+	<meta name="author" content="Grupo iFisica.org">
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<title>Painel POPE</title>
+</head>
+<body>
+	<h1>Painel Pope</h1>
+
+	<h3>Bem, vindo Usuário!</h3>
+</body>
+</html>
